bcf6de1b1ec4621d9b7f3af5962ffbe4037614df
merttoptas <merttoptas34@gmail.com>
2018-11-26 18:05:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mertcantoptas/sliderintro/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mertcantoptas.sliderintro.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mertcantoptas.sliderintro.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
